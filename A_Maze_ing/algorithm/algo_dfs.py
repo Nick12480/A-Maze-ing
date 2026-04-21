e986aa72b258aa5270eebf66e50511261c2efc23
Mika Meyer <mimeyer@c2r12s9.42wolfsburg.de>
2026-04-21 15:34:26 +0200

    removed animate, ready for algorithm

--- A_Maze_ing/algorithm/algo_dfs.py
+++ A_Maze_ing/algorithm/algo_dfs.py
@@ -4,7 +4,10 @@ from typing import Generator, Optional, Any, Callable
 import random
 import time
 
-from states import (
+import functools
+
+from .base import Algorithm
+from .states import (
     W,
     S,
     E,
@@ -25,7 +28,9 @@ from states import (
     OUTPUT_FILE,
     PERFECT,
     ALGORITHM,
-    TIME
+    TIME,
+    RED,
+    COLOR
 )
 
 
@@ -38,7 +43,7 @@ def timer(func: Callable) -> Callable:
     return wrapper
 
 
-class Maze:
+class Dfs(Algorithm):
     """
     look for random undiscovered area next to current
     explore, repeat
@@ -47,86 +52,9 @@ class Maze:
     if nearby block is = NESW its undiscovered
     """
 
-    def __init__(self, config: dict[str: Any]):
-        self.config = config
+    
 
     @timer
     def run(self):
         pass
 
-
-class Animate:
-
-    @staticmethod
-    def animate(output: str, char: str) -> None:
-        """
-        receive standard output as str to generate maze fully
-        based on given data, works independently
-        """
-        entry = ""
-        exit = ""
-        path = ""
-        height = 0
-        out_list = [item for item in output.split('\n')]
-        width = len(out_list[0])
-        for i, item in enumerate(out_list):
-            if item == '':
-                height = i
-                entry = out_list[i + 1]
-                exit = out_list[i + 2]
-                path = out_list[i + 3]
-                break
-
-        entry = (int(item) for item in entry.split(','))
-        exit = (int(item) for item in exit.split(','))
-
-        buff = Animate.__animate_build_maze(out_list, height, width, char)
-
-    @staticmethod
-    def __animate_build_maze(out_list: list, height: int, width: int, char: str) -> list:
-        buff = []
-        top = ""
-        curr = ""
-        for y in range(height):
-            for x in range(width):
-                if Animate.isbit(int(out_list[y][x], 16), N):
-                    top += char + char
-                    print(char + char, end='', file=sys.stdout, flush=True)
-                else:
-                    top += char + " "
-                    print(char + " ", end='', file=sys.stdout, flush=True)
-                time.sleep(TIME)
-
-            buff.append(top + char)
-            print(char)
-            top = ""
-
-            for x in range(width):
-                if Animate.isbit(int(out_list[y][x], 16), W):
-                    curr += char + " "
-                    print(char + " ", end='', file=sys.stdout, flush=True)
-                else:
-                    curr += "  "
-                    print("  ", end='', file=sys.stdout, flush=True)
-                time.sleep(TIME)
-
-            buff.append(curr + char)
-            print(char)
-            curr = ""
-
-        buff.append(char * (width * 2 + 1))
-        os.system('cls' if os.name == 'nt' else 'clear')
-
-        for i in buff:
-            print(i)
-
-    @staticmethod
-    def isbit(num: int, bit: int) -> bool:
-        return (num & bit) == bit
-
-
-if __name__ == '__main__':
-
-    with open('output.txt', 'r') as f:
-        # print(f.read())
-        Animate.animate(f.read(), '█')
