45b1dd334f37a61ba820abc54ec3ee406348d6ea
Mika Meyer <mimeyer@c2r12s9.42wolfsburg.de>
2026-04-21 15:28:34 +0200

    refractored Maze class to have one instance call all algorithms

---
+++ A_Maze_ing/maze.py
@@ -0,0 +1,22 @@
+from typing import Any, Optional
+
+
+
+from .algorithm import Dfs, Sidewinder, Algorithm, ALGORITHM
+from .animate import Animate
+
+
+class Maze:
+
+    def __init__(self, config: dict[str: Any]):
+        self.config = config
+
+    def run(self) -> Optional[Algorithm]:
+        match self.config[ALGORITHM]:
+            case 'sidewinder':
+                sidewinder = Sidewinder(self.config)
+                sidewinder.run()
+                return sidewinder
+            case 'dfs':
+                dfs = Dfs(self.config)
+                dfs.run()
